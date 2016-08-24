88c97a7da97a77d3cb9b262dd3e33a030ef50b09
Tommygun7468 <bathurstlivewire@hotmail.com>
2016-08-24 18:39:55 +1000

    Add files via upload
    Loading the level tutorial

--- Assessment 2.js
+++ Assessment 2.js
@@ -40,6 +40,34 @@ var fpsTime = 0;
 var player = new Player();
 var keyboard = new Keyboard();
 
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+
+function drawMap()
+{
+	for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
+	{
+		var idx = 0;
+		for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+		{
+			for( var x = 0; x < level1.layers[layerIdx].width; x++ )
+			{
+				if( level1.layers[layerIdx].data[idx] != 0 )
+				{
+					// the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+					// correct tile
+					var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+					var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+					var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_X)) * (TILESET_TILE + TILESET_SPACING);
+					context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+				}
+				idx++;
+			}
+		}
+	}
+}
+
 function run()
 {
 	context.fillStyle = "#ccc";		
